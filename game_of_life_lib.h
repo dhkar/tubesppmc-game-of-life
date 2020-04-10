ebcb3a833b10fc3be72bbaccfe03bed1ed96bd9a
Andhika R <dkrahard@gmail.com>
2020-04-11 00:06:13 +0700

    add Animate(), delay(), tick(), after merging branch

--- game_of_life_lib.h
+++ game_of_life_lib.h
@@ -6,7 +6,7 @@
 *   Asisten (NIM) : Arief Hirmanto (13217076)
 *   Nama File : game_of_life_lib.h
 *   Deskripsi : file library untuk conway's game_of_life
-*   Catatan : Second Commit, penambahan fungsi getSeedData dan destroyArray
+*   Catatan : add function Animate(), delay(), tick(), sudah di merge
 *           
 */
 #include <stdio.h>
@@ -44,21 +44,26 @@
         // Proses traversal file eksternal dan menyimpan setiap character ke array seedTemp[][]
         int i = 0; int j = 0;
         while((i != M-1) || (j != N-1)) {
+
             c = fgetc(*file_pointer);
             seedTemp[i][j] = c;
             ++j;
+
             if(j>N-1) {
+
                 c = fgetc(*file_pointer);
                 j = 0;
                 ++i;
 
             }
+
             if((i == M-1) && (j==N-1)){
+
                 c = fgetc(*file_pointer);
                 seedTemp[i][j] = c;
+
             }
 
-            
         }
 
         // Menyimpan dimensi world dari seed pada pointer x_dim dan y_dim
@@ -81,109 +86,112 @@
 
 
     /* Fungsi tick untuk menyimpan 1 generasi setelah generasi yang menjadi parameter
-* I.S. terdapat array 2D (seedData) yang memiliki alokasi memori sebagai parameter 
-* fungsi, panjang dan lebar array seedData
-* F.S. mengembalikan data berupa array 2D dynamic yang berisi data seed 1 generasi setelah generasi yang menjadi parameter
-*/
+    * I.S. terdapat array 2D (seedData) yang memiliki alokasi memori sebagai parameter 
+    * fungsi, panjang dan lebar array seedData
+    * F.S. mengembalikan data berupa array 2D dynamic yang berisi data seed 1 generasi setelah generasi yang menjadi parameter
+    */
 
-char **tick(char **seedData, int *x_dim, int *y_dim)
-{
-    int neighbors;
-    int x, y;
-    int rows;
-    int cols;
-    int i, j;
-    int rows_temp,cols_temp;
-    x = *x_dim;
-    y = *y_dim;
-
-    // alokasi memori array dinamis 2D newseedTemp (sesuai dengan ukuran)
-    char **newseedTemp;
-    newseedTemp = malloc(x * sizeof(*newseedTemp));
-    for (int i = 0; i < x; i++) {
-        newseedTemp[i] = malloc(y * sizeof(newseedTemp[0]));
-    }
-
-    // Hitung jumlah tetangga lalu ganti nilai seedData ke generasi selanjutnya
-    for (rows = 0; rows < y; rows++)
+    char **tick(char **seedData, int *x_dim, int *y_dim)
     {
-        rows_temp = 0;cols_temp = 0;
-        neighbors = 0;
+        int neighbors;
+        int x, y;
+        int rows;
+        int cols;
+        int i, j;
+        int rows_temp,cols_temp;
+        x = *x_dim;
+        y = *y_dim;
 
-        for (cols = 0; cols < x; cols ++)
+        // alokasi memori array dinamis 2D newseedTemp (sesuai dengan ukuran)
+        char **newseedTemp;
+        newseedTemp = malloc(x * sizeof(*newseedTemp));
+        for (int i = 0; i < x; i++) {
+            newseedTemp[i] = malloc(y * sizeof(newseedTemp[0]));
+        }
+
+        // Hitung jumlah tetangga lalu ganti nilai seedData ke generasi selanjutnya
+        for (rows = 0; rows < y; rows++)
         {
             rows_temp = 0;cols_temp = 0;
             neighbors = 0;
-            newseedTemp[rows][cols] = seedData[rows][cols];
 
-            if (seedData[rows][cols] == 'X')
+            for (cols = 0; cols < x; cols ++)
             {
-                for (i = -1; i <= 1; i++)
+                rows_temp = 0;cols_temp = 0;
+                neighbors = 0;
+                newseedTemp[rows][cols] = seedData[rows][cols];
+
+                if (seedData[rows][cols] == 'X')
                 {
-                    for (j = -1; j <= 1; j++)
+                    for (i = -1; i <= 1; i++)
                     {
+                        for (j = -1; j <= 1; j++)
+                        {
 
-                        rows_temp = rows +i;
-                        cols_temp = cols +j;
-                        if (rows_temp < 0){rows_temp = y-1;}
-                        if (cols_temp < 0){cols_temp = x-1;}
-                        if (rows_temp > y-1){rows_temp = 0;}
-                        if (cols_temp > x-1){cols_temp = 0;}
-             
-                        if ((seedData[rows_temp][cols_temp] == 'X')){
-                            neighbors++;}
+                            rows_temp = rows +i;
+                            cols_temp = cols +j;
+                            if (rows_temp < 0){rows_temp = y-1;}
+                            if (cols_temp < 0){cols_temp = x-1;}
+                            if (rows_temp > y-1){rows_temp = 0;}
+                            if (cols_temp > x-1){cols_temp = 0;}
+                
+                            if ((seedData[rows_temp][cols_temp] == 'X')){
+                                neighbors++;}
 
+                        }
                     }
+
+                    neighbors -= 1;
+
+                    if ((neighbors == 2) || (neighbors == 3)){
+                        newseedTemp[rows][cols] = 'X';} 
+                    else if ((neighbors < 2) || (neighbors > 3)){
+                        newseedTemp[rows][cols] = '-';}
+                        
                 }
 
-                neighbors -= 1;
-
-                if ((neighbors == 2) || (neighbors == 3)){
-                    newseedTemp[rows][cols] = 'X';} 
-                else if ((neighbors < 2) || (neighbors > 3)){
-                    newseedTemp[rows][cols] = '-';}
-                    
-            }
-
-            if (seedData[rows][cols] == '-')
-            {
-                for (i = -1; i <= 1; i++)
+                if (seedData[rows][cols] == '-')
                 {
-                    for (j = -1; j <= 1; j++){
-                        rows_temp = rows +i;
-                        cols_temp = cols +j;
-                        if (rows_temp < 0){rows_temp = y-1;}
-                        if (cols_temp < 0){cols_temp = x-1;}
-                        if (rows_temp > y-1){rows_temp = 0;}
-                        if (cols_temp > x-1){cols_temp = 0;}
+                    for (i = -1; i <= 1; i++)
+                    {
+                        for (j = -1; j <= 1; j++){
+                            rows_temp = rows +i;
+                            cols_temp = cols +j;
+                            if (rows_temp < 0){rows_temp = y-1;}
+                            if (cols_temp < 0){cols_temp = x-1;}
+                            if (rows_temp > y-1){rows_temp = 0;}
+                            if (cols_temp > x-1){cols_temp = 0;}
 
-                        if ((seedData[rows_temp][cols_temp] == 'X')){
-                            neighbors++;}
+                            if ((seedData[rows_temp][cols_temp] == 'X')){
+                                neighbors++;}
+                        }
                     }
+
+                    if (neighbors == 3){
+                        newseedTemp[rows][cols] = 'X';} 
+                    else if ((neighbors < 2) || (neighbors > 3)){
+                        newseedTemp[rows][cols] = '-';}
+                        
                 }
-
-                if (neighbors == 3){
-                    newseedTemp[rows][cols] = 'X';} 
-                else if ((neighbors < 2) || (neighbors > 3)){
-                    newseedTemp[rows][cols] = '-';}
-                    
-                    
             }
-        
 
-    /* Fungsi delay
+            return newseedTemp;
+
+        }
+
+    /* Fungsi delay()
         I.S. terdapat parameter milli_seconds dari input user yang merupakan waktu dalam 
         satuan ms
         F.S. mengembalikan data berupa delay selama input 
     */
-    void delay(int milli_seconds) 
+    void delay(int milli_seconds)
     { 
         int ms =  milli_seconds;
         clock_t start_time = clock(); 
         while (clock() < start_time + ms);
     } 
 
-    /* Fungsi delay
+    /* Fungsi Animate
         I.S. terdapat parameter file_pointer dari seedfile dan dimensi sumbu x, dimensi sumbu y
              dalam bentuk pointer to integer untuk menyimpan jumlah baris dan kolom, serta
              num_of_ticks untuk mengetahui jumlah iterasi tick
@@ -192,11 +200,14 @@ char **tick(char **seedData, int *x_dim, int *y_dim)
     void Animate(char **seedData, int* x_dim, int* y_dim, int num_of_ticks){
         char **newseedData;
         int k=0, x = *x_dim, y = *y_dim;
+
         newseedTemp = malloc(x * sizeof(*newseedTemp));
         for (int i = 0; i < x; i++) {
             newseedTemp[i] = malloc(y * sizeof(newseedTemp[0]));
         }
+
         newseedData = tick(seedData,&x,&y);
+
         while (k<num_of_ticks)
         {
             displaySeed(newseedData,&x,&y);
@@ -204,11 +215,7 @@ char **tick(char **seedData, int *x_dim, int *y_dim)
             newseedData = tick(newseedData,&x,&y);
             system("@cls||clear");
             k++;
-
         }
     }
-    return newseedTemp;
-}
-
 
 #endif
