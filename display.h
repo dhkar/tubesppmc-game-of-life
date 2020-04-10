ee56b4fb9543e600f573a8b3a6db1034605eeb91
sabrinadline <60213657+sabrinadline@users.noreply.github.com>
2020-04-11 01:13:56 +0700

    ngikutin display.c yang baru

--- display.h
+++ display.h
@@ -1,3 +1,15 @@
-void displaySeed();
-void displayMenu(int *game_over);
+/*  EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+*   MODUL 9 – TUGAS BESAR
+*   Kelompok : 3
+*   Rombongan : A
+*   Hari dan Tanggal : Jumat, 10 April 2020
+*   Asisten (NIM) : Arief Hirmanto (13217076)
+*   Nama File : display.h
+*   Deskripsi : fungsi displaySeed, displayInterface, dan displayMenu
+*   Catatan : memperbaiki ketiga fungsi
+*/
+
+char **displaySeed(char **seedData, int *x_dim, int *y_dim);
+int displayMenu();
 void displayInterface();
+
