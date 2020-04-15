5c9ce824f35b5458614d902e01e924895c2abb0d
sabrinadline <60213657+sabrinadline@users.noreply.github.com>
2020-04-16 00:15:31 +0700

    nambahin I.S dan F.S

--- display.c
+++ display.c
@@ -1,18 +1,24 @@
 /*  EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
-*   MODUL 9 – TUGAS BESAR
+*   MODUL 9 - TUGAS BESAR
 *   Kelompok : 3
 *   Rombongan : A
-*   Hari dan Tanggal : Jumat, 10 April 2020
+*   Hari dan Tanggal : Rabu, 15 April 2020
 *   Asisten (NIM) : Arief Hirmanto (13217076)
 *   Nama File : display.c
 *   Deskripsi : fungsi displaySeed, displayInterface, dan displayMenu
-*   Catatan : memperbaiki ketiga fungsi, masih terdapat bug pada case, masih ada beberapa testcase yang salah jika input saat quit
+*   Catatan : menambahkan I.S dan F.S
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-char **displaySeed(char **seedData, int *x_dim, int *y_dim){
+/* Fungsi displaySeed digunakan untuk mencetak array seed yang sudah terproses
+    I.S. terdapat array seed 2D yang sudah melalui proses animate atau tick
+    F.S. array 2D tersebut dicetak sehingga terlihat dalam cmd
+*/
+char **displaySeed(char **seedData, int *x_dim, int *y_dim)
+{
     int i;
     int j;
     for(i=0; i<=*y_dim; i++){
@@ -23,7 +29,12 @@ char **displaySeed(char **seedData, int *x_dim, int *y_dim){
     }
 }
 
-int displayMenu(){
+/* Fungsi displayMenu digunakan untuk mencetak menu pada user
+    I.S. setelah program meminta user memasukkan nama file seed
+    F.S. user memasukkan input pilihan pada display menu dan program menjalankan fungsi sesuai pilihan user
+*/
+int displayMenu()
+{
     int pilihan;
     printf("\n\nPilihan menu: \n");
     printf("1. Animate\n");
@@ -34,7 +45,12 @@ int displayMenu(){
     return pilihan;
 }
 
-void displayInterface(){
+/* Fungsi displayInterface digunakan untuk mencetak judul program atau sebagai header game
+    I.S. saat program pertama kali dijalankan
+    F.S. judul program akan dicetak pada terminal selama beberapa detik kemudian program meminta inputan file seed
+*/
+void displayInterface()
+{
     printf(":::       ::: :::::::::: :::         ::::::::   ::::::::  ::::    ::::  ::::::::::   :::::::::::  ::::::::\n");
     printf(":+:       :+: :+:        :+:        :+:    :+: :+:    :+: +:+:+: :+:+:+ :+:              :+:     :+:    :+:\n");
     printf("+:+       +:+ +:+        +:+        +:+        +:+    +:+ +:+ +:+:+ +:+ +:+              +:+     +:+    +:+\n");
@@ -58,5 +74,11 @@ void displayInterface(){
     printf("+#+   +#+# +#+     +#+ +#+       +#+ +#+         +#+    +#+ +#+         +#+            +#+     +#+        +#+        \n");
     printf("#+#    #+# #+#     #+# #+#       #+# #+#         #+#    #+# #+#         #+#            #+#     #+#        #+#        \n");
     printf(" ########  ###     ### ###       ### ##########   ########  ###         ########## ########### ###        ########## \n");
+
+    int ms =  3000;
+    clock_t start_time = clock();
+    while (clock() < start_time + ms);
+
+    system("@cls||clear");
 }
 
